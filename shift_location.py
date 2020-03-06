c44c3fa0b75e8ebeae0179eecc4dbc1219ca8156
oyvindjr-sigvaldsen <oyvindjr.sigvaldsen@me.com>
2020-03-06 23:21:33 +0100

    Directory management

---
+++ shift_location.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+
+# imports
+import os, sys
+
+absolute_path  = "/Users/o/Sites/github-repositories/planday-shift-schedule-PA/misc"
+sys.path.append(os.path.abspath(absolute_path))
+
+import master_schedule as ms
+
+def main():
+
+	
+
+if __name__ == "__main__":
+	main()
