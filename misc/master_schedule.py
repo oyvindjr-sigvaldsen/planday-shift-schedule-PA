200923d55b7584ab7b2b5fd6a28230d406bc1dc6
oyvindjr-sigvaldsen <oyvindjr.sigvaldsen@me.com>
2020-03-06 23:12:25 +0100

    Added master_schedule.py & misc dir

---
+++ misc/master_schedule.py
@@ -0,0 +1,162 @@
+master_schedule = [
+						{"Monday" :
+							[
+								{"Kannik" : [
+												["Elite & Junior/Senior", ["05", "30"], ["17", "15"]],
+												["Junior Ung Jenter", ["16", "00"], ["18", "30"]],
+												["Rekrutt Ung Jenter", ["17", "00"], ["18", "30"]],
+												["Hai 1", ["17", "00"], ["18", "30"]],
+												["Rekrutt Jenter", ["18", "00"], ["19", "30"]],
+												["Hai 2", ["18", "00"], ["19", "30"]],
+												["Medley Junior/Senior", ["19", "00"], ["20", "30"]]
+											]
+								},
+								{"Stavanger" : [
+												["Junior Ung Pluss trening", ["06", "00"], ["07", "30"]],
+												["Elite", ["16", "00"], ["19", "00"]]
+											]
+								},
+								{"Hundvåg" : [
+												["Junior Ung Gutter", ["16", "00"], ["19", "00"]]
+											]
+								},
+								{"Kvernevik" : [
+													["Junior/Senior", ["16", "00"], ["19", "00"]],
+													["Rekrutt Ung Gutter", ["17", "30"], ["19", "00"]],
+													["Dreamteam", ["18", "00"], ["20", "00"]],
+													["Rekrutt Gutter", ["18", "30"], ["20", "30"]]
+												]
+								}
+							],
+						},
+						{"Tuesday" :
+							[
+								{"Kannik" : [
+												["Elite", ["05", "30"], ["07", "15"]]
+											]
+								},
+								{"Stavanger" : [
+													["Junior/Senior", ["06", "00"], ["07", "30"]]
+												]
+								},
+								{"Hetland" : [
+												["Junior Ung Gutter", ["16", "00"], ["18", "30"]],
+												["Junior Ung Jenter", ["17", "30"], ["20", "00"]],
+												["Medley Junior Ung 2", ["19", "30"], ["21", "00"]]
+											]
+								},
+								{"Hundvåg" : [
+												["Medley Rekrutt", ["18", "00"], ["19", "30"]],
+												["Medley Junior/Senior", ["19", "00"], ["21", "00"]]
+											]
+								},
+								{"Kvernevik" : [
+													["Elite", ["16", "00"], ["18", "30"]],
+													["Junior/Senior", ["17", "30"], ["20", "00"]],
+													["Medley Junior Ung 1", ["19", "30"], ["21", "00"]]
+												]
+								}
+							]
+						},
+						{"Wednesday" :
+							[
+								{"Kannik" : [
+												["Elite & Junior/Senior", ["05", "30"], ["07", "15"]],
+												["Rekrutt Jenter", ["15", "30"], ["17", "00"]],
+												["Rekrutt Ung Jenter", ["17", "00"], ["18", "30"]],
+												["Medley Junior Ung 1", ["18", "00"], ["19", "30"]],
+												["Dreamteam", ["18", "45"], ["20", "00"]]
+											]
+								},
+								{"Stavanger" : [
+													["Junior Ung Jenter & Gutter", ["06", "00"], ["07", "30"]]
+												]
+								},
+								{"Hetland" : [
+												["Rekrutt Gutter", ["15", "30"], ["17", "00"]],
+												["Hai 1", ["16", "00"], ["17", "30"]],
+												["Rekrutt Ung Gutter", ["17", "00"], ["18", "30"]],
+												["Hai 2", ["16", "30"], ["18", "00"]],
+												["Medley Junior Ung 3 & Medley Junior/Senior", ["17", "30"], ["19", "00"]]
+											]
+								}
+							]
+						},
+						{"Thursday" :
+							[
+								{"Kannik" : [
+												["Elite", ["05", "30"], ["07", "15"]],
+												["Elite", ["16", "00"], ["19", "00"]],
+												["Junior/Senior", ["17", "00"], ["20", "00"]]
+											]
+								},
+								{"Hundvåg" : [
+												["Junior Ung Jenter", ["16", "00"], ["18", "30"]],
+												["Junior Ung Gutter", ["16", "30"], ["19", "00"]],
+												["Rekrutt Gutter", ["18", "30"], ["20", "00"]]
+											]
+								},
+								{"Kvernevik" : [
+												["Junior/Senior", ["06", "00"], ["07", "30"]],
+												["Rekrutt Jenter", ["18", "30"], ["20", "30"]],
+												["Hai 1 & Hai 2", ["18", "30"], ["20", "00"]]
+											]
+								}
+							]
+						},
+						{"Friday" :
+							[
+								{"Kannik" : [
+												["Elite & Junior Ung Gutter", ["05", "30"], ["17", "15"]],
+												["Elite", ["16", "00"], ["18", "30"]]
+											]
+								},
+								{"Hetland" : [
+												["Junior Ung Jenter", ["06", "00"], ["07", "30"]],
+												["Junior/Senior", ["16", "00"], ["18", "30"]],
+												["Medley Junior Ung 1", ["17", "30"], ["19", "00"]],
+												["Medley Junior/Senior", ["18", "30"], ["20", "00"]]
+											]
+								},
+								{"Kvernevik" : [
+													["Junior Ung Pluss Trening", ["17", "30"], ["20", "00"]],
+													["Rekrutt Pluss Trening", ["18", "00"], ["20", "00"]]
+												]
+								},
+								{"Hundvåg" : [
+												["Medley Rekrutt", ["17", "30"], ["19", "00"]],
+												["Medley Junior Ung 2", ["18", "30"], ["20", "00"]]
+											]
+								}
+							]
+						},
+						{"Saturday" :
+							[
+								{"Kannik" : [
+												["Elite", ["08", "00"], ["11", "00"]],
+												["Junior/Senior & Medley Junior/Senior", ["09", "00"], ["12", "00"]],
+												["Junior Ung Jenters", ["11", "00"], ["14", "00"]],
+												["Medley Junior Ung 3 + Medley Rekrutt", ["13", "30"], ["15", "00"]],
+												["Medley Junior Ung 2", ["15", "00"], ["17", "00"]],
+												["Medley Junior Ung 1", ["16", "00"], ["18", "00"]]
+											]
+								},
+								{"Hetland" : [
+												["Junior Ung Gutter", ["11", "00"], ["14", "00"]],
+												["Dreamteam", ["13", "45"], ["15", "00"]]
+											]
+								}
+							]
+						},
+						{"Sunday" :
+							[
+								{"Kannik" : [
+												["Rekrutt Jenter", ["10", "00"], ["12", "00"]],
+												["Rekrutt Ung Jenter & Rekrutt Ung Gutter", ["11", "00"], ["12", "30"]],
+												["Rekrutt Gutter", ["12", "00"], ["14", "00"]],
+												["Hai 1 & Hai 2 & Delfin", ["13", "30"], ["15", "00"]]
+											]
+								}
+							]
+						}
+					]
